38bbbc591d93583f5717006998e7efc6d7b03635
Alec Ventresca <aeventre@buffalo.edu>
2024-12-02 04:24:44 -0500

    i made more stupid changes

--- ros2_ws/src/gpio_sensors/gpio_sensors/gpio_sensor_node.py
+++ ros2_ws/src/gpio_sensors/gpio_sensors/gpio_sensor_node.py
@@ -7,7 +7,9 @@ class GpioSensorNode(Node):
     def __init__(self):
         super().__init__('gpio_sensor_node')
         self.gpio_pins = [29, 27, 31, 33, 35, 37]
-        self.foot_status_pub = self.create_publisher(Bool, 'foot_status', 10)
+        self.foot_status_pubs = [
+            self.create_publisher(Bool, f'foot_status_{pin}', 10) for pin in self.gpio_pins
+        ]
 
         GPIO.setmode(GPIO.BCM)
         for pin in self.gpio_pins:
@@ -16,22 +18,26 @@ class GpioSensorNode(Node):
         self.create_timer(0.5, self.publish_foot_status)
 
     def publish_foot_status(self):
-
-        foot_status = []
-        for pin in self.gpio_pins:
-            foot_status.append(GPIO.input(pin) == GPIO.HIGH)
-
-        msg = Bool()
-        msg.data = foot_status
-        self.foot_status_pub.publish(msg)
-        self.get_logger().info(f'Published foot status: {foot_status}')
-
+        for i, pin in enumerate(self.gpio_pins):
+            pin_state = GPIO.input(pin) == GPIO.HIGH
+            msg = Bool()
+            msg.data = pin_state
+            self.foot_status_pubs[i].publish(msg)
+            self.get_logger().info(f'Published foot status for pin {pin}: {pin_state}')
+        
 def main(args=None):
     rclpy.init(args=args)
     gpio_sensor_node = GpioSensorNode()
-    rclpy.spin(gpio_sensor_node)
-    gpio_sensor_node.destroy_node()
-    rclpy.shutdown()
+
+    try:
+        rclpy.spin(gpio_sensor_node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        gpio_sensor_node.destroy_node()
+        GPIO.cleanup()  # Clean up GPIO on shutdown
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
+
--- ros2_ws/src/gpio_sensors/setup.py
+++ ros2_ws/src/gpio_sensors/setup.py
@@ -5,17 +5,18 @@ package_name = 'gpio_sensors'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name]
-    install_requires=['setuptools', 'rclpy', 'RPi.GPIO'],
+    packages=[package_name],
+    install_requires=['setuptools', 'rclpy', 'RPi.GPIO', 'std_msgs'],
     zip_safe=True,
     maintainer='spiderbot',
     maintainer_email='ellahick@buffalo.edu',
-    description='A package to monitor GPIO sensors for foot states',
-    license='TODO: License declaration',
+    description='A package to monitor GPIO sensors for foot states.',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'gpio_sensor_node = gpio_sensors.gpio_sensor_node:main'
+            'gpio_sensor_node = gpio_sensors.gpio_sensor_node:main',
         ],
     },
+
 )
